0ba122fa9656471741ac33aff895feb845c56795
unknown <e1595070@FC411-MJPTCKT.cmaisonneuve.qc.ca>
2016-11-08 12:19:24 -0500

    mise à jour de la base de données

--- controleurs/Controleur_Admins.php
+++ controleurs/Controleur_Admins.php
@@ -1,47 +1,168 @@
 <?php
 	class Controleur_Admins extends BaseControleur
 	{	
-		//la fonction qui sera appelée par le routeur
+		//la fonction qui sera appelÃ©e par le routeur
 		public function traite(array $params)
 		{				
 			if(isset($params["action"]))
 			{
-				//modèle et vue vides par défaut
+				//modÃ¨le et vue vides par dÃ©faut
 				$data = array();
 				$vue = "";
-				//switch en fonction de l'action qui nous est envoyée
-				//ce switch détermine la vue $vue et obtient le modèle $data
+				//switch en fonction de l'action qui nous est envoyÃ©e
+				//ce switch dÃ©termine la vue $vue et obtient le modÃ¨le $data
 				switch($params["action"])
-				{
-									
-					case "insereFilm":
-						$modeleFilms = new Modele_Films();	
-						if(isset($params["titre"]) && isset($params["description"]) && isset($params["idRealisateur"]))
-						{
-							$valide = $modeleFilms->insereFilm($params["titre"], $params["description"], $params["idRealisateur"] );
-							if($valide)
-							{									
-								$this->afficheListeFilms();		
-							}
-							else
-							{
-								echo "ERROR";
-							}
-						}
-						else
-						{													
-							echo "ERROR";
-						}
-						break;										
+				{			
+                    case "importation":
+                    case "importation":
+						$this->importeOeuvreArtiste();
+						break;									
+                    case "importationArrondissements":
+						$this->importeArrondissements();
+						break;	
+					case "lienArtisteOeuvre":
+						$this->lienArtisteOeuvre();
+						break;	
+					case "lienOeuvreArrondissement":
+						$this->lienOeuvreArrondissement();
+						break;	
+                    case "lienOeuvreCategorie":
+						$this->lienOeuvreCate();
+						break;
+                  
 					default:
 						echo "ERROR";		
 				}						
 			}
 			else
 			{
-					//action par défaut
+					//action par dÃ©faut
 					echo "ERROR";					
 			}			
-		}	
+		} // fin de la fonction traite
+        
+        public function importeOeuvreArtiste()
+		{
+			$modeleAdmins = new Modele_admins();
+			$fichierJSON = file_get_contents('http://donnees.ville.montreal.qc.ca/dataset/2980db3a-9eb4-4c0e-b7c6-a6584cb769c9/resource/18705524-c8a6-49a0-bca7-92f493e6d329/download/oeuvresdonneesouvertes.json');
+			$fichierJSON_decode = JSON_decode($fichierJSON);
+			$compteur = count($fichierJSON_decode);
+			for($i = 0; $i < $compteur; $i++){
+				$noInterne			 = $fichierJSON_decode[$i]->NoInterne;
+				$titre 				 = utf8_decode($fichierJSON_decode[$i]->Titre);
+				$titreVariante       = utf8_decode($fichierJSON_decode[$i]->TitreVariante);
+				$nomCollection       = utf8_decode($fichierJSON_decode[$i]->NomCollection);
+				$categorieObjet      = utf8_decode($fichierJSON_decode[$i]->CategorieObjet);
+				$sousCategorieObjet  = utf8_decode($fichierJSON_decode[$i]->SousCategorieObjet);
+				$modeAcquisition     = utf8_decode($fichierJSON_decode[$i]->ModeAcquisition);
+				$dateAccession       = utf8_decode($fichierJSON_decode[$i]->DateAccession);
+				$materiaux           = utf8_decode($fichierJSON_decode[$i]->Materiaux);
+				$support             = utf8_decode($fichierJSON_decode[$i]->Support);
+				$technique           = utf8_decode($fichierJSON_decode[$i]->Technique);
+				$dimensionsGenerales = utf8_decode($fichierJSON_decode[$i]->DimensionsGenerales);
+				$parc                = utf8_decode($fichierJSON_decode[$i]->Parc);
+				$batiment            = utf8_decode($fichierJSON_decode[$i]->Batiment);
+				$adresseCivique      = utf8_decode($fichierJSON_decode[$i]->AdresseCivique);
+				$coordonneeLatitude  = utf8_decode($fichierJSON_decode[$i]->CoordonneeLatitude);
+				$coordonneeLongitude = utf8_decode($fichierJSON_decode[$i]->CoordonneeLongitude);
+				$numeroAccession     = utf8_decode($fichierJSON_decode[$i]->NumeroAccession);
+				$description         = "";
+				$urlImage            = "";
+			  
+				$modeleAdmins->insereOeuvre( 
+					$noInterne, 
+					$titre, 
+					$titreVariante, 
+					$nomCollection, 
+					$categorieObjet, 
+					$modeAcquisition, 
+					$dateAccession, 
+					$materiaux, 
+					$support, 
+					$technique, 
+					$dimensionsGenerales, 
+					$parc, 
+					$batiment, 
+					$adresseCivique, 
+					$coordonneeLatitude, 
+					$coordonneeLongitude, 
+					$numeroAccession, 
+					$description, 
+					$urlImage
+				);
+				
+				 $noInterneArtiste  =utf8_decode($fichierJSON_decode[$i]->Artistes[0]->NoInterne);
+				 $prenom            =utf8_decode($fichierJSON_decode[$i]->Artistes[0]->Prenom);
+				 $nom               =utf8_decode($fichierJSON_decode[$i]->Artistes[0]->Nom);
+				 $nomCollectif      =utf8_decode($fichierJSON_decode[$i]->Artistes[0]->NomCollectif);
+				 $modeleAdmins->insereArtiste($noInterneArtiste, $nom, $prenom, $nomCollectif);
+                
+				 $modeleAdmins->insereCategorie($sousCategorieObjet);
+                
+                
+                
+			}
+		} // fin de la fonction importeOeuvreArtiste
+		
+		
+		public function importeArrondissements()
+		{
+			$modeleAdmins = new Modele_admins();
+			$arron = file_get_contents('http://donnees.ville.montreal.qc.ca/dataset/00bd85eb-23aa-4669-8f1b-ba9a000e3dd8/resource/e9b0f927-8f75-458c-8fda-b5da65cc8b73/download/limadmin.json');
+			$arron_decode = JSON_decode($arron);
+			$compteur = count($arron_decode->features);
+			for($i = 0; $i < $compteur; $i++){
+				$ville = $arron_decode->features[$i]->properties->NOM;
+				$modeleAdmins->insereArrondissement(utf8_decode($ville));
+			}	
+		} // fin de la fonction importeArrondissements
+		
+		
+		public function lienArtisteOeuvre()
+		{
+			$modeleAdmins = new Modele_admins();
+			$fichierJSON = file_get_contents('http://donnees.ville.montreal.qc.ca/dataset/2980db3a-9eb4-4c0e-b7c6-a6584cb769c9/resource/18705524-c8a6-49a0-bca7-92f493e6d329/download/oeuvresdonneesouvertes.json');
+			$fichierJSON_decode = JSON_decode($fichierJSON);
+			$compteur = count($fichierJSON_decode);
+			for($i = 0; $i < $compteur; $i++){
+				$noInterneOeuvre	 =utf8_decode( $fichierJSON_decode[$i]->NoInterne);
+				$noInterneArtiste    =utf8_decode( $fichierJSON_decode[$i]->Artistes[0]->NoInterne);
+				$idOeuvre = $modeleAdmins->getIdSelonNoInterneO($noInterneOeuvre);
+				$idArtiste = $modeleAdmins->getIdSelonNoInterneA($noInterneArtiste);			
+				$modeleAdmins->insereLiens($idOeuvre["id"], $idArtiste["id"]);
+			}
+		} // fin de la fonction lienArtisteOeuvre
+		
+		
+		public function lienOeuvreArrondissement()
+		{
+			$modeleAdmins = new Modele_admins();
+			$fichierJSON = file_get_contents('http://donnees.ville.montreal.qc.ca/dataset/2980db3a-9eb4-4c0e-b7c6-a6584cb769c9/resource/18705524-c8a6-49a0-bca7-92f493e6d329/download/oeuvresdonneesouvertes.json');
+			$fichierJSON_decode = JSON_decode($fichierJSON);
+			$compteur = count($fichierJSON_decode);
+			for($i = 0; $i < $compteur; $i++){
+				$oeuvreArrondissement =$fichierJSON_decode[$i]->Arrondissement;
+				$noInterneOeuvre	 =utf8_decode( $fichierJSON_decode[$i]->NoInterne);
+				$idOeuvre = $modeleAdmins->getIdSelonNoInterneO($noInterneOeuvre);
+				$modeleAdmins->lienOeuvreArron($idOeuvre["id"], $oeuvreArrondissement);
+			}
+		} // fin de la fonction lienOeuvreArrondissement
+		
+			public function lienOeuvreCate()
+		{
+			$modeleAdmins = new Modele_admins();
+			$fichierJSON = file_get_contents('http://donnees.ville.montreal.qc.ca/dataset/2980db3a-9eb4-4c0e-b7c6-a6584cb769c9/resource/18705524-c8a6-49a0-bca7-92f493e6d329/download/oeuvresdonneesouvertes.json');
+			$fichierJSON_decode = JSON_decode($fichierJSON);
+			$compteur = count($fichierJSON_decode);
+			for($i = 0; $i < $compteur; $i++){
+				$oeuvreCategorie = utf8_decode($fichierJSON_decode[$i]->SousCategorieObjet);
+				$noInterneOeuvre	 =utf8_decode( $fichierJSON_decode[$i]->NoInterne);
+				$idOeuvre = $modeleAdmins->getIdSelonNoInterneO($noInterneOeuvre);
+				$modeleAdmins->lienOeuvreCategorie($idOeuvre["id"], $oeuvreCategorie);
+			}
+		} // fin de la fonction lienOeuvreCate
+        
+       
+		
 	}
 ?>
--- modeles/Modele_admins.php
+++ modeles/Modele_admins.php
@@ -6,12 +6,84 @@
 			return "admins";
 		}
 
-		public function insereRealisateur($prenom, $nom, $bio)
+		public function insereOeuvre($noInterne, 
+                                     $titre, 
+                                     $titreVariante, 
+                                     $nomCollection, 
+                                     $categorieObjet, 
+                                     $modeAcquisition, 
+                                     $dateAccession, 
+                                     $materiaux, 
+                                     $support, 
+                                     $technique, 
+                                     $dimensionsGenerales, 
+                                     $parc, 
+                                     $batiment, 
+                                     $adresseCivique, 
+                                     $coordonneeLatitude, 
+                                     $coordonneeLongitude, 
+                                     $numeroAccession, 
+                                     $description, 
+                                     $urlImage
+                                     )
 		{		
 			try
 			{
-				$stmt = $this->connexion->prepare("insert into realisatejhfhkxfykhurs(prenom, nom, bio) values (:prenom, :nom, :bio)");
-				$stmt->execute(array(":prenom" => $prenom, ":nom" => $nom, ":bio" => $bio));
+				$stmt = $this->connexion->prepare("INSERT INTO `oeuvre` (`noInterne`, `titre`, `titreVariante`, `nomCollection`, `categorieObjet`, `modeAcquisition`, `dateAccession`, `materiaux`, `support`, `technique`, `dimensionsGenerales`, `parc`, `batiment`, `adresseCivique`, `coordonneeLatitude`, `coordonneeLongitude`, `numeroAccession`, `description`, `urlImage`) VALUES (:noInterne, :titre, :titreVariante, :nomCollection, :categorieObjet, :modeAcquisition, :dateAccession, :materiaux, :support, :technique, :dimensionsGenerales, :parc, :batiment, :adresseCivique, :coordonneeLatitude, :coordonneeLongitude, :numeroAccession, :description, :urlImage)");
+				$stmt->execute(array("noInterne" => $noInterne, 
+                                     ":titre" => $titre, 
+                                     ":titreVariante" => $titreVariante, 
+                                     ":nomCollection" => $nomCollection, 
+                                     ":categorieObjet" => $categorieObjet, 
+                                     ":modeAcquisition" => $modeAcquisition, 
+                                     ":dateAccession" => $dateAccession, 
+                                     ":materiaux" => $materiaux,  
+                                     ":support" => $support, 
+                                     ":technique" => $technique, 
+                                     ":dimensionsGenerales" => $dimensionsGenerales, 
+                                     ":parc" => $parc, 
+                                     ":batiment" => $batiment, 
+                                     ":adresseCivique" => $adresseCivique, 
+                                     ":coordonneeLatitude" => $coordonneeLatitude, 
+                                     ":coordonneeLongitude" => $coordonneeLongitude, 
+                                     ":numeroAccession" => $numeroAccession, 
+                                     ":description" => $description, 
+                                     ":urlImage" => $urlImage
+                                     ));
+				return 1;
+			}	
+			catch(Exception $exc)
+			{
+				return 0;
+			}
+		}
+        
+        public function insereArtiste($noInterne, $nom, $prenom, $nomCollectif)
+		{		
+			try
+			{
+				$stmt = $this->connexion->prepare("INSERT INTO `artiste` (`noInterne`, `nom`, `prenom`, `nomCollectif`) VALUES (:noInterne, :nom, :prenom, :nomCollectif)");
+                
+				$stmt->execute(array(":noInterne" => $noInterne, 
+                                     ":nom" => $nom, 
+                                     ":prenom" => $prenom, 
+                                     ":nomCollectif" => $nomCollectif
+                                    ));
+				return 1;
+			}	
+			catch(Exception $exc)
+			{
+				return 0;
+			}
+		}
+        
+         public function insereCategorie($nom)
+		{		
+			try
+			{
+				$stmt = $this->connexion->prepare("INSERT INTO `categorie` ( `nom`) VALUES (:nom)");
+                
+				$stmt->execute(array(":nom" => $nom));
 				return 1;
 			}	
 			catch(Exception $exc)
@@ -20,5 +92,93 @@
 			}
 		}
 		
+		
+		public function insereArrondissement($nom)
+		{		
+			try
+			{
+				$stmt = $this->connexion->prepare("INSERT INTO `arrondissement` (`nom`) VALUES (:nom)");
+				$stmt->execute(array(":nom" => $nom));
+				return 1;
+			}	
+			catch(Exception $exc)
+			{
+				return 0;
+			}
+		}
+		
+		
+		public function getIdSelonNoInterneO($noInterne)
+		{		
+			try
+			{
+				$stmt = $this->connexion->prepare("SELECT id FROM oeuvre where noInterne = :noInterne ");
+				$stmt->execute(array(":noInterne" => $noInterne));
+				return $stmt->fetch();
+			}	
+			catch(Exception $exc)
+			{
+				return 0;
+			}
+		}
+		
+			
+		public function getIdSelonNoInterneA($noInterne)
+		{		
+			try
+			{
+				$stmt = $this->connexion->prepare("SELECT id FROM artiste where noInterne = :noInterne ");
+				$stmt->execute(array(":noInterne" => $noInterne));
+				return $stmt->fetch();
+			}	
+			catch(Exception $exc)
+			{
+				return 0;
+			}
+		}
+		
+		public function insereLiens($idOeuvre, $idArtiste)
+		{		
+			try
+			{
+				$stmt = $this->connexion->prepare("INSERT INTO `oeuvreArtiste` (`idOeuvre`, `idArtiste`) VALUES (:idOeuvre, :idArtiste)");
+				$stmt->execute(array(":idOeuvre" => $idOeuvre, ":idArtiste" => $idArtiste));
+				return 1;
+			}	
+			catch(Exception $exc)
+			{
+				return 0;
+			}
+		}
+		
+		public function lienOeuvreArron($idOeuvre, $arron)
+		{		
+			try
+			{
+				$stmt = $this->connexion->prepare("INSERT INTO `oeuvreArrondissement` (`idOeuvre`, `nomArrondissement`) VALUES (:idOeuvre, :arron)");
+				$stmt->execute(array(":idOeuvre" => $idOeuvre, ":arron" => $arron));
+				return 1;
+			}	
+			catch(Exception $exc)
+			{
+				return 0;
+			}
+		}
+        
+        public function lienOeuvreCategorie($idOeuvre, $categorie)
+		{		
+			try
+			{
+				$stmt = $this->connexion->prepare("INSERT INTO `oeuvrecategorie` (`idOeuvre`, `nomCategorie`) VALUES (:idOeuvre, :categorie)");
+				$stmt->execute(array(":idOeuvre" => $idOeuvre, ":categorie" => $categorie));
+				return 1;
+			}	
+			catch(Exception $exc)
+			{
+				return 0;
+			}
+		}
+      
 	}
 ?>
+
