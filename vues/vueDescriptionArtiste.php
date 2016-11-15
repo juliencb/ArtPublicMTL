ae38ebff83afaa20431c247c3e716ebcd3c7986d
unknown <e1595421@FC411-MJPTCKY.cmaisonneuve.qc.ca>
2016-11-15 11:17:13 -0500

    Requete page description d'un artiste

--- vues/vueDescriptionArtiste.php
+++ vues/vueDescriptionArtiste.php
@@ -4,18 +4,18 @@
 		<!--recherche ici -->
 	</aside>
 	<div id="descriptionArtiste">
-		<?php 
-
-		//var_dump($data);
-		
-			
-		?>
-				<h1>Biographie
-					<?php echo $data["prenom"] . " " . $data["nom"] . " " . $data["nomCollectif"]; ?>	
-				</h1>
-				<p><?php echo $data["biogrphie"] ?></p>
-			<!--<img src="">-->
+		<h1 class="bioArtiste">Biographie
+			<?php 
+				if (isset($data["prenom"]) && isset($data["nom"])){
+					echo $data["prenom"] . " " . $data["nom"]; 	
+				} elseif(isset($data["nomCollectif"])){
+					echo $data["nomCollectif"];
+				}
+			?>
+		</h1>
+		<p>
+			<?php echo $data["biographie"]; ?>
+		</p>
+	</div>
 	
-	</div>	
-</div>	
 
