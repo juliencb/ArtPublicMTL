45b2f056408e44b6dd3ce32987398922cf565878
Stéphanie Moreau <Stphanie Moreau>
2016-11-15 10:37:05 -0500

    correnction de bug sur la vue afficheDetails

--- modeles/Modele_usagers.php
+++ modeles/Modele_usagers.php
@@ -11,7 +11,7 @@
 		{		
 			try
 			{
-				$stmt = $this->connexion->prepare("select oeuvre.titre,oeuvre.categorieObjet,oeuvre.categorie,oeuvre.parc,oeuvre.materiaux,oeuvre.adresseCivique,oeuvre.urlImage,artiste.nom,artiste.prenom, arrondissement.nom as nomArrondissement from oeuvre 
+				$stmt = $this->connexion->prepare("select oeuvre.titre,oeuvre.categorieObjet,oeuvre.categorie,oeuvre.parc,oeuvre.materiaux,oeuvre.adresseCivique,oeuvre.urlImage,artiste.nom,artiste.prenom,artiste.nomCollectif, arrondissement.nom as nomArrondissement from oeuvre 
 				join oeuvreartiste on oeuvre.id = oeuvreartiste.idOeuvre 
 				join artiste on oeuvreartiste.idArtiste = artiste.id join arrondissement on oeuvre.arrondissement = arrondissement.nom where oeuvre.id = :id");
 				
--- vues/AfficheDetails.php
+++ vues/AfficheDetails.php
@@ -1,38 +1,59 @@
-
-﻿
 <div id="affichedata">
 	<h1>Affichage des détails de l'oeuvre</h1>
 	<?php
-	
-	
-	if($data["urlImage"]!=""){
-		?>
-			<img src = " ./images/<?php echo $data["urlImage"]?>.jpg" alt = " <?php echo $data["titre"] ?>"/><br><br>
-		<?php
-	}
-	else{
-		?>
-		<img src = "./images/no-image-available.jpg" alt = "aucune image"/><br><br>
-		<?php
-		
-	}
-	
-	
+		if($data["urlImage"]!=""){
+			?>
+				<img src = " ./images/<?php echo $data["urlImage"]?>.jpg" alt = " <?php echo $data["titre"] ?>"/><br><br>
+			<?php
+		}
+		else{
 	?>
-
+			<img src = "./images/no-image-available.jpg" alt = "aucune image"/><br><br>
+	<?php
+	}
+	?>
+		<ul>	
+	<?php
+		if($data["titre"]!="")
+		{
 	
-			<ul>
-					<li> Titre : <?php echo $data["titre"]?></li>
-					<li> Artiste : <?php echo $data["prenom"]." ".$data["nom"]?></li>
-					<li> Catégorie :<?php echo $data["categorieObjet"] ?></li>
-					<li> Sous - Catégorie : <?php echo $data["categorie"] ?></li>
-					<li> Parc : <?php echo $data["parc"] ?></li>
-					<li> Matériaux : <?php echo $data["materiaux"] ?></li>
-					<li> Adresse : <?php echo $data["adresseCivique"] ?></li>
-					<li> Arrondissement : <?php echo $data["nomArrondissement"] ?></li>
-					
-			</ul>	
-				
-				
-
+			echo "<li> Titre :"." ". $data["titre"]. "</li>";
+		}	
+		
+		 if(($data["prenom"]&&$data["nom"]!="")&&($data["prenom"]!= null && $data["nom"]!= null ))
+		 {
+			echo "<li>Artiste : "." ". $data["prenom"]." ".$data["nom"]. "</li>";
+	
+		}
+		else{
+			echo "<li>Atelier : "." ". $data["nomCollectif"]."</li>";
+		}
+		if($data["categorieObjet"]!="")
+		{
+			echo "<li> Catégorie :"." ". $data["categorieObjet"]. "</li>";
+		}	
+		if($data["categorie"]!="")
+		{
+			echo "<li> Sous-Catégorie :"." ". $data["categorie"]. "</li>";
+		}	
+		if($data["parc"]!="")
+		{
+			echo "<li> Parc :"." ". $data["parc"]. "</li>";
+	
+		}
+		if($data["materiaux"]!="")
+		{
+			echo "<li> Matériaux :"." ". $data["materiaux"]. "</li>";
+	
+		}	
+		if($data["adresseCivique"]!="")
+		{
+			echo "<li> Adresse :"." ". $data["adresseCivique"]. "</li>";
+		}		
+		if($data["nomArrondissement"]!="")
+		{
+			echo "<li> Arrondissement :"." ". $data["nomArrondissement"]. "</li>";
+		}	
+			?>	
+	</ul>	
 </div>
