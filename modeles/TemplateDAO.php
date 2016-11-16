1e9d3d7d771c0d123d7a5a463dc8b9230a2d95e8
TAPE HUBERSON VINCENT DE PAUL <TAPE HUBERSON VINCENT DE PAUL>
2016-11-16 18:17:35 -0500

    correction du nom de la base de donnees

--- modeles/TemplateDAO.php
+++ modeles/TemplateDAO.php
@@ -16,8 +16,7 @@
 			try
 			{
 
-
-				$this->connexion = new PDO("mysql:dbname=demoartpublic;host=localhost", "root", "");
+				$this->connexion = new PDO("mysql:dbname=artpublicmtl;host=localhost", "root", "");
 
 			}
 			catch(Exception $exc)
