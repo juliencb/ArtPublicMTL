803a4a531dc104c901da4997e3d7662706b54cfd
unknown <e1595421@FC411-MJPTCKY.cmaisonneuve.qc.ca>
2016-11-30 14:08:07 -0500

    Liste artistes en ordre alphabetique, séparer par la lettre

--- vues/vueArtistes.php
+++ vues/vueArtistes.php
@@ -8,23 +8,38 @@
 			<h1>Artistes</h1>
 				<ul>
 					<?php 
-                        for($i = "a"; $i <= "z"; $i++){ ?>
-                            <h2><?php echo $i; ?></h2>
-                            <?php  
-                            foreach ($data as $artiste){
-                                if($artiste["prenom"]!="Inconnu"){      
-                            ?>
-                                    <li>
-                                        <a href="index.php?Public&action=descriptionArtiste&id=<?php echo $artiste['id'] ?>">
-                                            <?php echo $artiste["prenom"] . " " . $artiste["nomArtiste"]; ?>
+                        $compteur = 65;
+                    ?>
+                        <h1>A</h1>
+                    <?php 
+                        foreach ($data as $artiste){
+                            $premiereLettre = substr($artiste["nomArtiste"], 0, 1);
+                            
+                            if(chr($compteur) != $premiereLettre){
+                                $compteur++;
+                        ?>
+                        <h1><?php echo chr($compteur); ?></h1>
+                    <?php    
+                            }
+                            if(($artiste["prenom"]!="Inconnu") && (chr($compteur) == $premiereLettre)){   ?>    
+                                <li>
+                                    <a href="index.php?Public&action=descriptionArtiste&id=<?php echo $artiste['id'] ?>">
+                                        <?php echo $artiste["prenom"] . " " . $artiste["nomArtiste"]; ?>
 
-                                        </a>
-                                    </li>
+                                    </a>
+                                </li>
                         <?php 
                                 }//fermeture if
                             } //fermeture foreach
-						} //fermeture fo
-					   ?>
+                            if($compteur < 91){
+                                do{
+                                $compteur++;
+                        ?>
+                        <h1><?php echo chr($compteur); ?></h1>
+                    <?php        
+                                }while($compteur < 90) ;
+                            }
+				    ?>
 				</ul>
 		</section>
 	</div>
